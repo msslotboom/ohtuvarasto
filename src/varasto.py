79f3f1b69b799b0d79398d775baaea0bfc8d116a
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-11-14 20:36:39 +0200

    Another action test commit

--- src/varasto.py
+++ src/varasto.py
@@ -41,7 +41,7 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-
+    
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
