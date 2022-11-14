e4c4021b3f4d9a7ccce3f65b10851a813bfcaa0a
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-11-14 19:16:50 +0200

    Viikko 2 Tehtävä 3

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -40,6 +41,7 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
+
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
